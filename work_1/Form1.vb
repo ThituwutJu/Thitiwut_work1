1b0c306cf249da47921bea9b61c1081c2215197b
masyan455 <109448816+masyan455@users.noreply.github.com>
2022-08-07 16:40:53 +0700

    Button_cal

--- work_1/Form1.vb
+++ work_1/Form1.vb
@@ -1,3 +1,29 @@
 ﻿Public Class Form1
-
+    Private Sub btcal_Click(sender As Object, e As EventArgs) Handles btcal.Click
+        Dim base, height, area As Double
+        If tbbase.Text = "" Then
+            MsgBox("ใส่ข้อมูลความกว้างของฐาน", MsgBoxStyle.Exclamation, "ข้อผิดพลาด")
+            tbbase.Focus()
+        Else
+            If IsNumeric(tbbase.Text) Then
+                base = Val(tbbase.Text)
+                If tbheight.Text = "" Then
+                    MsgBox("ใส่ข้อมูลความสูง", MsgBoxStyle.Exclamation, "ข้อผิดพลาด")
+                    tbheight.Focus()
+                Else
+                    If IsNumeric(tbheight.Text) Then
+                        height = Val(tbheight.Text)
+                        area = 0.5 * base * height
+                        lboutput.Text = area.ToString
+                    Else
+                        MsgBox("ใส่ข้อมูลความสูงเป็นตัวเลขเท่านั้น", MsgBoxStyle.Exclamation, "ข้อผิดพลาด")
+                        tbbase.Focus()
+                    End If 'check numeric height
+                End If 'check blank height
+            Else
+                MsgBox("ใส่ข้อมูลความกว้างของฐานเป็นตัวเลขเท่านั้น", MsgBoxStyle.Exclamation, "ข้อผิดพลาด")
+                tbbase.Focus()
+            End If 'check numeric base
+        End If 'check blank base
+    End Sub
 End Class
