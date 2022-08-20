5a1bf6fe58c03def90a7a59f4d5292d2420cb07f
ThituwutJu <109449343+ThituwutJu@users.noreply.github.com>
2022-08-20 20:44:28 +0700

    Interface

---
+++ Thitiwut_work1/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class form1
+    Private Sub delete(sender As Object, e As EventArgs) Handles btdelete.Click
+
+    End Sub
+
+    Private Sub save(sender As Object, e As EventArgs) Handles btsave.Click
+
+    End Sub
+
+    Private Sub ListBox1_Click(sender As Object, e As EventArgs) Handles ListBox1.Click
+
+    End Sub
+
+    Private Sub clearall(sender As Object, e As EventArgs) Handles btallclr.Click
+
+    End Sub
+
+    Private Sub add(sender As Object, e As EventArgs) Handles btadd.Click
+
+    End Sub
+
+    Private Sub clear(sender As Object, e As EventArgs) Handles btclr.Click
+
+    End Sub
+
+    Private Sub bt_exit(sender As Object, e As EventArgs) Handles btexit.Click
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+End Class
